423edacac99862ecbb4138cfb15b3ba35384ab4e
Alejandro Carrau <acarrau@users.noreply.github.com>
2017-06-02 14:01:27 -0300

    Remove jquery dependency

--- scripts/moduleHandler.js
+++ scripts/moduleHandler.js
@@ -1,8 +1,8 @@
 function runModule(modulePath) {
-    $("#console").html("");
+    document.getElementById("console").innerHTML = "";
     
     // Show original code in iframe
-    $("#script-source").attr("src", "modules/" + modulePath);
+    document.getElementById("script-source").src = "modules/" + modulePath;
 
     // Actually execute module's transpiled code
     var script = document.createElement("script")
@@ -10,10 +10,13 @@ function runModule(modulePath) {
     document.getElementsByTagName('head')[0].appendChild(script);        
 }
 
+// console.log redefinition:
+//
 // Redefine the global console.log function to 
 // print the output from the modules in the console div
 var old = console.log;
 console.log = function (line) {
-    $("#console").append("<span>" + line + "</span><br />")
+    document.getElementById("console").innerHTML += "<span>" + line + "</span><br />"
+    // Call old console.log method
     old(line)
 }
