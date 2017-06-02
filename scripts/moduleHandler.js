aac197ac38ac0d88ff5c72a1cab635f48112217f
Alejandro Carrau <acarrau@users.noreply.github.com>
2017-06-02 13:51:23 -0300

    Redefine console.log output to print in an html element

--- scripts/moduleHandler.js
+++ scripts/moduleHandler.js
@@ -1,4 +1,12 @@
 function runModule(modulePath) {
     $("#console").html("");
-    $("#script-source").attr("src", modulePath);
+    $("#script-source").attr("src", "modules/" + modulePath);
+}
+
+// Redefine the global console.log function to 
+// print the output from the modules in the console div
+var old = console.log;
+console.log = function (line) {
+    $("#console").append("<span>" + line + "</span><br />")
+    old(line)
 }
