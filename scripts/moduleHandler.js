09cf10038a20f2a6670ff18df16a2209b2e8b548
Alejandro Carrau <acarrau@users.noreply.github.com>
2017-06-02 10:08:58 -0300

    Make transpiled code to run on browser and not node on trivial host app.

---
+++ scripts/moduleHandler.js
@@ -0,0 +1,9 @@
+var moduleIdToScriptMap = {
+    1: "1-scopes.js",
+    2: "2-decorators.js"
+}
+
+function runModule(moduleId) {
+    var modulePath = "modules/" + moduleIdToScriptMap[moduleId]
+    $("#code").load(modulePath);
+}
